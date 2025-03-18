c7200e5730ad221348f117365218fc5309faf7e5
Preston Rowley <preston.k.rowley@gmail.com>
2025-03-18 11:38:12 -0500

    Added notes on functionality.

--- src/main.py
+++ src/main.py
@@ -3,3 +3,16 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+'''
+Notes for how this ideally should run:
+booting the program should check the previous saved state and load the checked ranges (t-eff, d) and individual stars checked vs what is downloaded.
+    if something is downloaded and unchecked, start there
+    if nothing is available, start at the edge t range and download data.
+check new downloaded range for fits files if they exist.
+    if they do, start processing.
+        update the save state with completed files and stars.
+    else, expand the range again.
+once that range has been checked, update save state.
+'''
